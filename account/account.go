e77f065e67a3385784dafc42fe977d177a4533cd
Henry Baldwin <henry@stork.network>
2025-12-30 09:44:03 -0500

    Remove tyler-smith go-bip39 in favor of cosmos owned fork.

--- account/account.go
+++ account/account.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/coming-chat/go-aptos/crypto/derivation"
 	"github.com/coming-chat/go-sui/v2/sui_types"
-	"github.com/tyler-smith/go-bip39"
+	"github.com/cosmos/go-bip39"
 	"golang.org/x/crypto/blake2b"
 )
 
